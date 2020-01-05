1cbc003f2826d10f0d31294410b28db87808d922
José Luís Oliveira da Cunha <joseloc300-dev@outlook.com>
2020-01-05 23:21:47 +0000

    A1 best A1

--- scripts/ct_scanner.py
+++ scripts/ct_scanner.py
@@ -312,60 +312,20 @@ def createModel():
 def createModelA1():
     model = Sequential()
 
-    model.add(Conv3D(64, (3, 3, 3), input_shape=(80,80,80,1), activation='relu'))
-    model.add(BatchNormalization())
-    model.add(MaxPooling3D(pool_size=(2, 2, 2)))
-    model.add(Dropout(0.25))
-
-    model.add(Conv3D(128, (3, 3, 3), activation='relu'))
-    model.add(BatchNormalization())
-    model.add(MaxPooling3D(pool_size=(2, 2, 2)))
-    model.add(Dropout(0.25))
-
-    model.add(Conv3D(256, (3, 3, 3), activation='relu'))
-    model.add(BatchNormalization())
-    model.add(MaxPooling3D(pool_size=(2, 2, 2)))
-    model.add(Dropout(0.25))
-
-    print(model.output_shape)
-    model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
-    print(model.output_shape)
-
-    model.add(Dense(4096, activation='relu'))
-    model.add(Dropout(0.50))
-
-    # Output layer
-    model.add(Dense(3), activation='softmax')
-
-    model.compile(Adam(lr=.0001), loss='categorical_crossentropy', metrics=['accuracy'])
-    
-    return model
-
-# for masked cubes size 80
-def createModelA2():
-    model = Sequential()
-
     model.add(Conv3D(12, (3, 3, 3), input_shape=(80,80,80,1), activation='relu'))
-    #model.add(BatchNormalization())
     model.add(MaxPooling3D(pool_size=(2, 2, 2)))
-    #model.add(Dropout(0.25))
 
     model.add(Conv3D(24, (3, 3, 3), activation='relu'))
-    #model.add(BatchNormalization())
     model.add(MaxPooling3D(pool_size=(2, 2, 2)))
-    #model.add(Dropout(0.25))
 
     model.add(Conv3D(48, (3, 3, 3), activation='relu'))
-    #model.add(BatchNormalization())
     model.add(MaxPooling3D(pool_size=(2, 2, 2)))
-    #model.add(Dropout(0.25))
 
     print(model.output_shape)
     model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
     print(model.output_shape)
 
     model.add(Dense(128, activation='relu'))
-    #model.add(Dropout(0.50))
 
     # Output layer
     model.add(Dense(3, activation='softmax'))
@@ -373,7 +333,6 @@ def createModelA2():
     model.compile(Adam(lr=.0001), loss='categorical_crossentropy', metrics=['accuracy'])
     
     return model
-
 
 def main():
     # list available gpus in order to limit memory allocation
@@ -402,7 +361,7 @@ def main():
     #print("done")
     #time.sleep(20)
 
-    cubeList = loadMiniCubes(-1) # change between loadMiniCubes and loadMaskedCubes
+    cubeList = loadMaskedCubes(-1) # change between loadMiniCubes and loadMaskedCubes
     textures = getTextures(-1)
 
     validationSplit = 0.3
@@ -410,14 +369,14 @@ def main():
 
     valid_cube_list_training, valid_cube_list_validation, valid_textures_training, valid_textures_validation = parseTrainingData(cubeList, textures, validationSplit, cubeSize)
 
-    model = createModel()
-    #model = tf.keras.models.load_model('../models/model')
+    model = createModelA2()
+    #model = tf.keras.models.load_model('../models/modelA2')
 
 
-    model.fit(valid_cube_list_training, valid_textures_training, batch_size=16, epochs=32, validation_data=(valid_cube_list_validation, valid_textures_validation))
+    model.fit(valid_cube_list_training, valid_textures_training, batch_size=4, epochs=8, validation_data=(valid_cube_list_validation, valid_textures_validation))
 
     # folder needs to exist before instruction is ran
-    model.save('../models/model')
+    model.save('../models/modelA2')
 
     to_predict = np.array([valid_cube_list_validation[0]]).reshape(-1, cubeSize, cubeSize, cubeSize, 1)
     predictions = model.predict(to_predict)
